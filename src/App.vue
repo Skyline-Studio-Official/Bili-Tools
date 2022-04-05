93289e3ebbd95621cdbb2f01b7d9ae61e376b2ad
Your Name <you@example.com>
2022-04-05 13:32:00 +0800

    2022.04.05 | 13:32 | 搭建框架完毕

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <a-layout style="height:100vh">
     <a-layout-header style="color:white">
-      这是脑袋
+      这是Header
+      <!-- Router-Link = <a> 但是这是针对路由的 -->
+      <div style="" class="nav">
+        <div class="nav-item"><router-link to="/" >主页</router-link></div>
+        <div class="nav-item"><router-link to="/world" >世界</router-link></div>
+      </div>
     </a-layout-header>
     <a-layout>
-      <a-layout-sider style="color:white">
+      <!-- <a-layout-sider style="color:white">
         这是侧边栏
-      </a-layout-sider>
+      </a-layout-sider> -->
       <a-layout-content>
         <!-- Router View会渲染此时页面的路由 -->
+        <!-- 渲染的东西会放在Router-View标签里 -->
         <router-view></router-view>
       </a-layout-content>
     </a-layout>
@@ -20,6 +26,17 @@
 
 </script>
 
-<style>
-
+<style #scope>
+.nav {
+  display: inline-block;
+  margin-left:60px;
+}
+.nav-item {
+  display: inline-block;
+  margin-left: 40px;
+  color:white;
+}
+router-link {
+  color: white;
+}
 </style>
