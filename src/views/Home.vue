5cd91411243a80f6fd1cb2801e63ac0996c185ef
Your Name <you@example.com>
2022-04-05 19:50:33 +0800

    2022.04.05 | 19:50 | 最新代码

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,46 @@
 <template>
   <div class="home">
-    <a-button type="primary">Welcome</a-button>
+    <a-button type="primary" @click="checkConn">Welcome</a-button>
   </div>
 </template>
 
 <script>
 export default {
   mounted () {
-    fetch('http://localhost:8866/demo.php').then(res => {
-        return res.json()
-    }).then(res => {
-        console.log(res)
-    }).catch(function (error) { // 请求失败处理
-        console.log(error);
-    });
+    this.checkConn()
+  },
+  data() {
+    return {
+      temp_code: 0, // 验证连接状态码
+      temp_msg:0, // 验证连接状态信息
+    }
   },
   methods: {
-    info() {
-      this.$message.info('This is a normal message');
+    checkConn(){
+      fetch('http://localhost:8866/demo.php').then(res => {
+          return res.json()
+      }).then(res => {
+          console.log(res)
+          console.log(this.temp_code)
+          this.temp_code = res.code;
+          console.log(this.temp_code)
+          this.temp_msg = res.msg;
+          console.log(this.temp_code)
+          
+      }).catch(function (error) { // 请求失败处理
+          console.log(error);
+      })
+      if(this.temp_code == 20000){
+        this.success("恭喜！后端连接成功，您可以畅玩啦！")
+      } else {
+        this.error("抱歉，后端连接失败，请检查您的网络连接")
+      }
+    },
+    success(data) {
+      this.$message.success(data);
+    },
+    error(data) {
+      this.$message.error(data);
     }
   }
 }
