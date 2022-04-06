881bb05f688a0d006013dd7a91162caf881cb984
Your Name <you@example.com>
2022-04-06 14:45:46 +0800

    2022.04.06 | 14:45 | 重新提交因为GIT出错的代码

--- back/phpinfo.php
+++ back/phpinfo.php
@@ -1,12 +1,8 @@
 <?php
-<<<<<<< HEAD
-phpinfo();
-=======
     /*
      * Author : torunwithwind 和风赛跑的男人
      * Action : phpinfo测试
      * Time : 2022/4/6
      * */
     phpinfo();
->>>>>>> d1be2a531a8af0f05f9154f9ee46275b5ba3645a
 ?>
